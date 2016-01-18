7304ae83ad1553c71dfe40e1aca420f63ebf939e
robertoallende <rallende@gmail.com>
2016-01-18 19:48:59 -0300

    Refactor: removing fab icon from list fragment.

--- app/src/main/java/com/robertoallende/uiskel/ItemListActivity.java
+++ app/src/main/java/com/robertoallende/uiskel/ItemListActivity.java
@@ -27,16 +27,6 @@ public class ItemListActivity extends AppCompatActivity implements
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
