82c5cc6151595830c56b2922c2f4fd43b473c5b7
robertoallende <rallende@gmail.com>
2016-01-20 20:37:21 -0300

    style: removing unused imports.

--- app/src/main/java/com/robertoallende/uiskel/ItemListActivity.java
+++ app/src/main/java/com/robertoallende/uiskel/ItemListActivity.java
@@ -2,14 +2,9 @@ package com.robertoallende.uiskel;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-
 import com.robertoallende.uiskel.dummy.DummyContent;
 
 import java.util.List;
--- app/src/main/java/com/robertoallende/uiskel/ItemListFragment.java
+++ app/src/main/java/com/robertoallende/uiskel/ItemListFragment.java
@@ -5,13 +5,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
 import com.robertoallende.uiskel.dummy.DummyContent;
 import com.robertoallende.uiskel.dummy.DummyContent.DummyItem;
 
